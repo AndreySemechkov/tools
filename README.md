08819360952726c3f326ad971fd85bfdf4dafc76
Andrey Semetchkov <24417389+AndreySemechkov@users.noreply.github.com>
2023-09-08 19:17:33 +0300

    added openai API scripts, added README

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# mytools
-Utilities and usefull scripts
+# 🚀 My Automation Repository 🛠️
+
+Welcome my Automation Repository!🤖
+
+## Table of Contents 📚
+
+- [Introduction](#introduction)
+- [Scripts](#scripts)
+  - [MySQL](#mysql)
+  - [OpenAI](#openai)
+- [Getting Started](#getting-started)
+
+## Introduction 📝
+
+In today's fast-paced world of DevOps, automation is key to saving time and reducing errors. This repository contains a collection of scripts and configurations that will help you automate various DevOps tasks.
+
+## Scripts 📜
+
+
+### MySQL 🐬
+
+The `mysql` directory is your go-to place for MySQL-related automation:
+
+- `binlog.backup.config.template`: Template for configuring binlog_backup.sh script.
+- `binlog_backup.sh`: Script to perform MySQL binlog backups.
+- `download_audit_log.py`: Python script for downloading AWS RDS MySQL audit logs.
+- `myloader_mydumper`: A Dockerized solution for logical backup and restore at scale of MySQL databases. Utilizing: azcopy, mysql, mydumper, myloader, percona-toolkit and Dockerfile.  
+  - `Dockerfile`: Dockerfile for creating my portable MySQL toolkit container.
+  - `mysqlkit.sh`: CLI toolkit for MySQL database backup and restore. The MySQL Toolkit 🚀 supports the following commands:
+    - help: Display the script's usage and available options.
+    - backup: Create backups of AWS RDS DB MySQL tables and upload them to a Blob Store.
+    - restore: Restore MySQL tables from Blob Storage.
+    - test_script: Test the script and display some diagnostic information.
+    - run_command: Execute SQL commands or MySQL Client commands.
+    - restore_command: Execute restore and run_command sequentially.
+
+### OpenAI 🧠
+
+Need an AI assistant in your workflow? Check out the `openai` directory:
+
+- `chat.py`: Python script for integrating OpenAI's GPT-3 API into your automation.
+- `translate.py`: Python script for language translation using OpenAI's language models.
+
+
+## License 📄
+
+This repository is licensed under the [MIT License](LICENSE), which means you can use, modify, and distribute the code as you see fit.
+
+Feel free to contribute, open issues, or reach out if you have any questions or suggestions ! 🌟🤖🌟
---
+++ openai/chat.py
@@ -0,0 +1,55 @@
+# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+import openai
+import os
+import click
+
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+assert OPENAI_API_KEY, ("OPENAI_API_KEY env variable not set")
+models = ["gpt-3.5-turbo", "gpt-4", "gpt-4-32k"]
+
+class Context:
+    def __init__(self, role, content):
+        self.role = role
+        self.content = content
+    
+    def to_dict(self):
+        return {'role': self.role, 'content': self.content} # type: ignore
+
+def ai_answer(prompt, format, style, model):
+    CONTEXT = []
+    msgs = [
+         {"role": "system", "content": f"Format the answers {format}"},
+         {"role": "system", "content": f"You are {style}"},
+    ]
+    CONTEXT.append(Context(role="user", content=prompt).to_dict())
+    msgs.extend(CONTEXT)
+    openai.api_key = OPENAI_API_KEY
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=msgs
+    )
+    response_txt = response["choices"][0]['message']['content'] # type: ignore
+    CONTEXT.append(Context(role="assistant", content=response_txt).to_dict())
+    return response_txt
+
+@click.command(help="Use OpenAI API via your OPENAI_API_KEY to get the AI to answer the prompts and ask the user for the next prompt.\n \
+                     When the user types \"exit\" the chat should exit.\n")
+@click.option('--prompt', default='', help='Enter a smart prompt')
+@click.option('--format', default='', help='Enter the format that the answers should follow')
+@click.option('--style', default='Jim Carrey in Ace Ventura', help='Describe the character/role of the AI that the answers will reflect, default is answering in character of Ace Ventura The Pet Detective')
+@click.option('--model', default="gpt-3.5-turbo", help=f'Choose an LLM: {models}')
+def chat(prompt, format, style, model):
+    # use OpenAI API to get the answer to the prompts print the answer and ask the user for the next prompt
+    # when the user types "exit" the chat should exit
+    if model not in models:
+        print(f"Wrong OpenAI LLM model. Choose from: {models}")
+        exit(1)
+    if not prompt:
+        prompt = input("Enter your prompt: ")
+    while prompt != "exit":
+        print(ai_answer(prompt, format, style, model))
+        prompt = input("Enter your prompt: ")
+
+if __name__ == "__main__":
+    chat()

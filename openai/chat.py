88934737687640a0f89915574257132514b49c36
Andrey Semetchkov <andreys@qwilt.com>
2024-01-03 18:08:20 +0200

    read prompt from file

--- openai/chat.py
+++ openai/chat.py
@@ -30,23 +30,29 @@ def ai_answer(prompt, format, style, model):
     CONTEXT.append(Context(role="assistant", content=response_txt).to_dict())
     return response_txt
 
-@click.command(help="Use OpenAI API via your OPENAI_API_KEY to get the AI to answer the prompts and ask the user for the next prompt.\n \
-                     When the user types \"exit\" the chat should exit.\n")
-@click.option('--prompt', default='', help='Enter a smart prompt')
+def read_prompt(format, style, model):
+    prompt = ""
+    while prompt != "exit":
+        prompt = input("Enter your prompt: ")
+        if prompt.startswith("/"):
+            with open(prompt, "r") as file:
+                prompt = file.read()
+            print(f"{prompt}\n")
+        print(ai_answer(prompt, format, style, model))
+
+@click.command(help="Use OpenAI API via your OPENAI_API_KEY to ask OpenAI models to answer the prompts interactively and ask the user for the next prompt.\n"
+                    "You will be asked to enter a smart prompt string, or for long prompts enter a full file path starting with \"/\". For example \"/Users/andreys/code/playground/prompt\".\n"
+                    "When the user types \"exit\", the chat should exit.")
 @click.option('--format', default='', help='Enter the format that the answers should follow')
-@click.option('--style', default='Jim Carrey in Ace Ventura', help='Describe the character/role of the AI that the answers will reflect, default is answering in character of Ace Ventura The Pet Detective')
-@click.option('--model', default="gpt-3.5-turbo", help=f'Choose an LLM: {models}')
-def chat(prompt, format, style, model):
+@click.option('--style',  default='Jim Carrey in Ace Ventura', help='Describe the character/role of the AI that the answers will reflect, default is answering in character of Ace Ventura The Pet Detective')
+@click.option('--model',  default="gpt-3.5-turbo", help=f'Choose an LLM: {models}')
+def chat(format, style, model):
     # use OpenAI API to get the answer to the prompts print the answer and ask the user for the next prompt
     # when the user types "exit" the chat should exit
     if model not in models:
         print(f"Wrong OpenAI LLM model. Choose from: {models}")
         exit(1)
-    if not prompt:
-        prompt = input("Enter your prompt: ")
-    while prompt != "exit":
-        print(ai_answer(prompt, format, style, model))
-        prompt = input("Enter your prompt: ")
+    read_prompt(format, style, model)
 
 if __name__ == "__main__":
     chat()
